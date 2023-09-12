f949dcde79df888ac2663b0e4ad76bf9e67a949f
Dwane Hemmings <dwane.hemmings@vonage.com>
2023-09-12 00:28:58 -0400

    Add files via upload

---
+++ static/host.js
@@ -0,0 +1,54 @@
+/* global OT */
+const publisherEl = document.querySelector('video-publisher');
+const login = document.querySelector('#login');
+const dashboard = document.querySelector('#dashboard');
+const nameInput = document.querySelector('#name');
+const enterBtn = document.querySelector('#enter-button');
+const leaveBtn = document.querySelector('#leave-button');
+const chatEl = document.querySelector('video-chat');
+
+let apiKey;
+let sessionId;
+let token;
+let session;
+
+function handleError(error) {
+  if (error) {
+    console.error(error);
+    alert(error);
+  }
+}
+
+async function initializeSession() {
+  console.log('initializeSession');
+  session = OT.initSession(apiKey, sessionId);
+  publisherEl.session = session;
+  publisherEl.token = token;
+  chatEl.session = session;
+  chatEl.username = nameInput.value;
+  login.style.display = 'none';
+  dashboard.style.display = 'flex';
+
+  leaveBtn.addEventListener('click', () => {
+    session.disconnect();
+    dashboard.style.display = 'none';
+    nameInput.value = '';
+    login.style.display = 'block';
+  });
+}
+
+enterBtn.addEventListener('click', () => {
+  // Make a GET request to get the Vonage Video API key, Session ID, and Token from the server
+  fetch('/session')
+    .then((response) => response.json())
+    .then((json) => {
+      apiKey = json.apiKey;
+      sessionId = json.sessionId;
+      token = json.token;
+      // Initialize an OpenTok Session object
+      console.log('apiKey: ', apiKey);
+      console.log('sessionId: ', sessionId);
+      console.log('token: ', token);
+      initializeSession();
+    });
+});
---
+++ static/index.js
@@ -0,0 +1,53 @@
+/* global OT */
+const subscribersEl = document.querySelector('video-subscribers');
+const login = document.querySelector('#login');
+const dashboard = document.querySelector('#dashboard');
+const nameInput = document.querySelector('#name');
+const enterBtn = document.querySelector('#enter-button');
+const leaveBtn = document.querySelector('#leave-button');
+const chatEl = document.querySelector('video-chat');
+
+let apiKey;
+let sessionId;
+let token;
+let session;
+
+function handleError(error) {
+  if (error) {
+    console.error(error);
+    alert(error);
+  }
+}
+
+async function initializeSession() {
+  console.log('initializeSession');
+  session = OT.initSession(apiKey, sessionId);
+  subscribersEl.session = session;
+  subscribersEl.token = token;
+  chatEl.session = session;
+  console.log('nameInput.value: ', nameInput.value);
+  chatEl.username = nameInput.value;
+
+  login.style.display = 'none';
+  dashboard.style.display = 'flex';
+
+  leaveBtn.addEventListener('click', () => {
+    session.disconnect();
+    dashboard.style.display = 'none';
+    nameInput.value = '';
+    login.style.display = 'block';
+  });
+}
+
+enterBtn.addEventListener('click', () => {
+  // Make a GET request to get the OpenTok API key, session ID, and token from the server
+  fetch('/watch')
+    .then((response) => response.json())
+    .then((json) => {
+      apiKey = json.apiKey;
+      sessionId = json.sessionId;
+      token = json.token;
+      // Initialize an OpenTok Session object
+      initializeSession();
+    });
+});
